a238ca303951b57477e19c6d34bbe96a789dc21d
gabe-digg <167433859+gabe-digg@users.noreply.github.com>
2025-03-14 18:28:39 +0000

    newer
    Signed-off-by: gabe-digg <167433859+gabe-digg@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@
 #include "keypad.h"
 #include "mbed.h"
 #include "SLCD.h"
-#include <iostream>
 // Blinking rate in milliseconds
 #define BLINKING_RATE 600ms
 #define BLINKING_RATE2 200ms
 using ThisThread::sleep_for;
 
-Keypad MyKeypad(PTA12, PTA4, PTA5, PTC8, PTD3, PTA2, PTA1);
+Keypad MyKeypad( PTA12,PTA4, PTA5, PTC8, PTD3, PTA2, PTA1);
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 Thread thread1;
 Thread thread2;
 Thread thread3;
 SLCD screen;
+Ticker my_timer;
 char pressedkey;        
 void led1_thread()
 {
@@ -37,17 +37,21 @@ void keypad_thread(){
     while (true){
         pressedkey = MyKeypad.ReadKey();
         if (pressedkey!=NO_KEY) {
-            screen.printf("I-%02x", pressedkey);
-            std::cout << pressedkey << std::endl;
+            //screen.printf("I-%02x", pressedkey);
+            screen.printf("%d", pressedkey);
+
         }
         sleep_for(BLINKING_RATE);
     }
 }
-int main(void) {
+int main() {
+screen.clear();
+screen.Home();
 thread1.start(led1_thread);
 thread2.start(led2_thread);
 thread3.start(keypad_thread);
  while (true) { // Added infinite loop to keep main running
+ printf("%c",MyKeypad.ReadKey()); // to test if the key press is coming through
         sleep_for(1s); // Prevent main from consuming too much CPU
 }
 }
